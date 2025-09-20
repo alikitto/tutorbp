6a802ce5e125e79cc4fa1d2fbdaa3c5dc67b3efc
alikitto <aleskerov.alekper@gmail.com>
2025-09-20 21:11:08 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 set -e
-
-# Устанавливаем mysql-client вручную (Railpack образ обычно Debian/Ubuntu)
-apt-get update -y && apt-get install -y default-mysql-client curl gzip
-
 bash scripts/entrypoint.sh
